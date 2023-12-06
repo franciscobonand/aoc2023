0f2aa5c1854231d7e46ce8fb382400d6af67f8ec
Rmgreco <rafaelmgreco@hotmail.com>
2023-12-06 00:33:10 -0300

    part 2

--- node/day1-js/index.js
+++ node/day1-js/index.js
@@ -4,19 +4,59 @@ function extractCalibrationValues(input) {
     const lines = input.split('\n');
     let sum = 0;
 
-    lines.forEach(line => {
-        const digits = line.match(/\d/g);
+    const numMap = {
+        one: '1',
+        two: '2',
+        three: '3',
+        four: '4',
+        five: '5',
+        six: '6',
+        seven: '7',
+        eight: '8',
+        nine: '9',
+        zero: '0'
+    };
 
-        if (digits && digits.length >= 2) {
-            const calibrationValue = parseInt(digits[0] + digits[digits.length - 1]);
-            sum += calibrationValue;
-        } else if (digits && digits.length >= 1) {
-            const calibrationValue = parseInt(digits.join('') + digits.join(''));
-            sum += calibrationValue;
+    const leftRegex = new RegExp(`(${Object.keys(numMap).join("|")})$`);
+  const rightRegex = new RegExp(`^(${Object.keys(numMap).join("|")})`);
+  const digitRegex = new RegExp(`^([0-9])`);
+
+  lines.forEach((line) => {
+    let strLeft = "";
+    let strRight = "";
+
+    let leftDigit = "";
+    let rightDigit = "";
+
+    let i = 0;
+    while (!leftDigit || !rightDigit) {
+      if (!leftDigit) {
+        strLeft += line[i];
+        if (line[i].match(digitRegex)) {
+          leftDigit = line[i];
+        } else if (strLeft.match(leftRegex)) {
+          leftDigit = numMap[strLeft.match(leftRegex)[0]];
         }
-    });
+      }
 
-    return sum;
+      if (!rightDigit) {
+        strRight = line[line.length - i - 1] + strRight;
+
+        if (line[line.length - i - 1].match(digitRegex)) {
+          rightDigit = line[line.length - i - 1];
+        } else if (strRight.match(rightRegex)) {
+          rightDigit = numMap[strRight.match(rightRegex)[0]];
+        }
+      }
+
+      i += 1;
+    }
+
+    const numStr = leftDigit + rightDigit;
+    sum += Number.parseInt(numStr);
+  });
+
+  return sum;
 }
 
 function readFile(filePath) {
@@ -29,7 +69,7 @@ function readFile(filePath) {
     }
 }
 
-const filePath = './input.txt'; 
+const filePath = './input.txt';
 const fileContent = readFile(filePath);
 const totalCalibration = extractCalibrationValues(fileContent.trim());
 console.log('Total Calibration:', totalCalibration);
